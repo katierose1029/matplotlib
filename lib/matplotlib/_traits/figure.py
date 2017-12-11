e47e2ee16de45aca374f4be4411f4b53da9a4ba0
neminem1203 <tpaul1203@gmail.com>
2017-12-11 16:02:54 -0500

    forgot to push. should be working

--- lib/matplotlib/_traits/figure.py
+++ lib/matplotlib/_traits/figure.py
@@ -1761,7 +1761,7 @@ class Figure(Artist, HasTraits):
 
 		# Store the value of gca so that we can set it back later on.
 		current_ax = self.gca()
-
+        #TODO: i commented this out. need to fix it so it works
 		#        if cax is None:
 		#            if use_gridspec and isinstance(ax, SubplotBase):
 		#                cax, kw = cbar.make_axes_gridspec(ax, **kw)
@@ -1923,4 +1923,66 @@ class Figure(Artist, HasTraits):
 				self, self.axes, subplotspec_list, renderer,
 				pad=pad, h_pad=h_pad, w_pad=w_pad, rect=rect)
 		self.subplots_adjust(**kwargs)
+
+
+def figaspect(arg):
+    """
+    Create a figure with specified aspect ratio.  If *arg* is a number,
+    use that aspect ratio.  If *arg* is an array, figaspect will
+    determine the width and height for a figure that would fit array
+    preserving aspect ratio.  The figure width, height in inches are
+    returned.  Be sure to create an axes with equal with and height,
+    e.g.,
+
+    Example usage::
+
+      # make a figure twice as tall as it is wide
+      w, h = figaspect(2.)
+      fig = Figure(figsize=(w,h))
+      ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+      ax.imshow(A, **kwargs)
+
+
+      # make a figure with the proper aspect for an array
+      A = rand(5,3)
+      w, h = figaspect(A)
+      fig = Figure(figsize=(w,h))
+      ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+      ax.imshow(A, **kwargs)
+
+    Thanks to Fernando Perez for this function
+    """
+
+    isarray = hasattr(arg, 'shape') and not np.isscalar(arg)
+
+    # min/max sizes to respect when autoscaling.  If John likes the idea, they
+    # could become rc parameters, for now they're hardwired.
+    figsize_min = np.array((4.0, 2.0))  # min length for width/height
+    figsize_max = np.array((16.0, 16.0))  # max length for width/height
+
+    # Extract the aspect ratio of the array
+    if isarray:
+        nr, nc = arg.shape[:2]
+        arr_ratio = float(nr) / nc
+    else:
+        arr_ratio = float(arg)
+
+    # Height of user figure defaults
+    fig_height = rcParams['figure.figsize'][1]
+
+    # New size for the figure, keeping the aspect ratio of the caller
+    newsize = np.array((fig_height / arr_ratio, fig_height))
+
+    # Sanity checks, don't drop either dimension below figsize_min
+    newsize /= min(1.0, *(newsize / figsize_min))
+
+    # Avoid humongous windows as well
+    newsize /= max(1.0, *(newsize / figsize_max))
+
+    # Finally, if we have a really funky aspect ratio, break it but respect
+    # the min/max dimensions (we don't want figures 10 feet tall!)
+    newsize = np.clip(newsize, figsize_min, figsize_max)
+    return newsize
+
+docstring.interpd.update(Figure=martist.kwdoc(Figure))
 
